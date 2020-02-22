204a96f480eff5b4271def08b674df151ab9d858
Suhel Rana <suhelrana53@gmail.com>
2020-02-22 17:25:20 +0600

    fixed settings.py file

--- weather/settings.py
+++ weather/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ir@5u952h!%15*r5be$0bows39mnhcmdhoyumbmsdio62tjc&^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -118,4 +118,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
